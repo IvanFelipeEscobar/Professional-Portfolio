f92741a3fa1748fddc1525d1cf4a0d0b53904141
Ivan Escobar <ivnescbr1989@gmail.com>
2023-03-25 14:44:09 -0400

    nav bar layout & functionality part 3

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -15,7 +15,7 @@ export default function PortfolioContainer() {
       if (currentPage === 'Home') {
         return <Home />;
       }
-      if (currentPage === 'About') {
+      if (currentPage === 'AboutMe') {
         return <AboutMe />;
       }
       if (currentPage === 'Contact') {
