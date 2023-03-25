25cfc769aa63e9ea8e53f4fb432630113c4df70b
Ivan Escobar <ivnescbr1989@gmail.com>
2023-03-25 14:11:54 -0400

    nav bar layout & functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,40 @@ import React from "react"
 function NavBar({currentPage, handlePageChange}){
     return(
         //nav bar html here
-        <ul></ul>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a 
+                    href="#home" onClick={()=>handlePageChange(`Home`)} 
+                    className={currentPage===`Home`?`nav-link active`:`nav-link`}
+                > 
+                Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a 
+                    href="#aboutme" onClick={()=>handlePageChange(`AboutMe`)} 
+                    className={currentPage===`AboutMe`?`nav-link active`:`nav-link`}
+                > 
+                Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a 
+                    href="#projects" onClick={()=>handlePageChange(`Projects`)} 
+                    className={currentPage===`Projects`?`nav-link active`:`nav-link`}
+                > 
+                Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <a 
+                    href="#contact" onClick={()=>handlePageChange(`Contact`)} 
+                    className={currentPage===`Contact`?`nav-link active`:`nav-link`}
+                > 
+                Home
+                </a>
+            </li>
+        </ul>
     )
 }
 
