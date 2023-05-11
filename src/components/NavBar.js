ab1846aed8888d98cfd2b0bb7a017f44d75c95b9
Ivan Escobar <ivnescbr1989@gmail.com>
2023-05-11 11:13:00 -0400

    still trying

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ const NavBar = () => {
     <Container>
      <Navbar.Brand>Ivan Felipe Escobar</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/" active>Home <FontAwesomeIcon icon={faHome} /></Nav.Link>
-        <Nav.Link href="/aboutme">About Me <FontAwesomeIcon icon={faHatWizard} /></Nav.Link>
-        <Nav.Link href="/projects">Projects <FontAwesomeIcon icon={faCode}/></Nav.Link>
-        <Nav.Link href="/contact">Contact Me <FontAwesomeIcon icon={faFileZipper} /></Nav.Link>
+        <Nav.Link to="/" active>Home <FontAwesomeIcon icon={faHome} /></Nav.Link>
+        <Nav.Link to="/aboutme">About Me <FontAwesomeIcon icon={faHatWizard} /></Nav.Link>
+        <Nav.Link to="/projects">Projects <FontAwesomeIcon icon={faCode}/></Nav.Link>
+        <Nav.Link to="/contact">Contact Me <FontAwesomeIcon icon={faFileZipper} /></Nav.Link>
       </Nav>
     </Container>
   </Navbar>
