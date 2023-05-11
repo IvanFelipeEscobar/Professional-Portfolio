f6c23e3569d3da3261128806f09927e24e798ad2
Ivan Escobar <ivnescbr1989@gmail.com>
2023-05-11 11:18:32 -0400

    still trying

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,17 @@
 import {Nav, Navbar, Container} from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faHatWizard, faHome, faFileZipper, faCode} from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-router-dom'
 const NavBar = () => {
     return(
     <Navbar bg="dark" variant="dark" expand="sm">
     <Container>
      <Navbar.Brand>Ivan Felipe Escobar</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link to="/" active>Home <FontAwesomeIcon icon={faHome} /></Nav.Link>
-        <Nav.Link to="/aboutme">About Me <FontAwesomeIcon icon={faHatWizard} /></Nav.Link>
-        <Nav.Link to="/projects">Projects <FontAwesomeIcon icon={faCode}/></Nav.Link>
-        <Nav.Link to="/contact">Contact Me <FontAwesomeIcon icon={faFileZipper} /></Nav.Link>
+        <Nav.Link active><Link to="/">Home <FontAwesomeIcon icon={faHome} /></Link></Nav.Link>
+        <Nav.Link><Link to="/aboutme">About Me <FontAwesomeIcon icon={faHatWizard} /></Link></Nav.Link>
+        <Nav.Link><Link to="/projects">Projects <FontAwesomeIcon icon={faCode}/></Link></Nav.Link>
+        <Nav.Link><Link to="/contact">Contact Me <FontAwesomeIcon icon={faFileZipper} /></Link></Nav.Link>
       </Nav>
     </Container>
   </Navbar>
