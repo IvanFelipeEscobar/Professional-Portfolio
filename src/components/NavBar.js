02997c8c1d321461b297b029676175b8f2738944
Ivan Escobar <ivnescbr1989@gmail.com>
2023-05-12 10:46:56 -0400

    reconfig Link => LinkContainer to maintain bootstrap element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import {Nav, Navbar, Container} from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faHatWizard, faHome, faFileZipper, faCode} from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 const NavBar = () => {
     return(
     <Navbar bg="dark" variant="dark" expand="sm">
     <Container>
      <Navbar.Brand>Ivan Felipe Escobar</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link><Link to="/">Home <FontAwesomeIcon icon={faHome} /></Link></Nav.Link>
-        <Nav.Link><Link to="/aboutme">About Me <FontAwesomeIcon icon={faHatWizard} /></Link></Nav.Link>
-        <Nav.Link><Link to="/projects">Projects <FontAwesomeIcon icon={faCode}/></Link></Nav.Link>
-        <Nav.Link><Link to="/contact">Contact Me <FontAwesomeIcon icon={faFileZipper} /></Link></Nav.Link>
+        <LinkContainer to="/"><Nav.Link>Home <FontAwesomeIcon icon={faHome} /></Nav.Link></LinkContainer>
+        <LinkContainer to="/aboutme"><Nav.Link>About Me <FontAwesomeIcon icon={faHatWizard} /></Nav.Link></LinkContainer>
+        <LinkContainer to="/projects"><Nav.Link>Projects <FontAwesomeIcon icon={faCode}/></Nav.Link></LinkContainer>
+        <LinkContainer to="/contact"><Nav.Link>Contact Me <FontAwesomeIcon icon={faFileZipper} /></Nav.Link></LinkContainer>
       </Nav>
     </Container>
   </Navbar>
