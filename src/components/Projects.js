a3de46fefb85b27a8a7387c5a0c6b7617d259a75
Ivan Escobar <ivnescbr1989@gmail.com>
2023-08-08 13:09:14 -0400

    carousel links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,21 +8,21 @@ const Projects = ({ projects }) => {
         <h1>Projects...</h1>
         <p></p>
       </div>
-      <Carousel
-        className="carousel"
-        indicators={false}
-        pause="hover"
-      >
+      <Carousel className="carousel" indicators={false} pause="hover">
         {projects.map((p) => (
           <Carousel.Item key={p.title}>
             <div className="carousel__item">
-              <img
-                className="projectPic"
-                src={process.env.PUBLIC_URL + p.src}
-                alt={p.title}
-              />
+              <a href={p.link}>
+                <img
+                  className="projectPic"
+                  src={process.env.PUBLIC_URL + p.src}
+                  alt={p.title}
+                />
+              </a>
 
-              <a href={p.link} className="carousel__item--project-title">{p.title}</a>
+              <a href={p.link} className="carousel__item--project-title">
+                {p.title}
+              </a>
               <p>{p.description}</p>
               <a href={p.github}>
                 <FontAwesomeIcon icon={faGithub} /> GitHub Repo
